a0cda5f777406e9a04d3897b99d7e260a8e36812
Andbet <andbet050197@gmail.com>
2017-03-08 22:20:35 -0500

    VHDL para el control de motor PaP

--- modulo2/Motorapasos.vhd
+++ modulo2/Motorapasos.vhd
@@ -20,16 +20,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Motor_a_pasos is
 	Port ( StepDrive : out std_logic_vector(3 downto 0);
 			 Direction : in std_logic;
@@ -37,44 +27,39 @@ entity Motor_a_pasos is
 			 CLK : in std_logic);
 end Motor_a_pasos;
 
---La variable direction como su nombre lo indica es para indicar la dirección a la que va a girar el motor, el StepEnable únicamente es un switch que activa o no el movimiento del motor , el reloj que es esencial para llevar un conteo de pasos y finalmente las salidas de los 4 pines.
---Para esto se requieren las siguientes variables temporales:
+--Direction: indica la dirección a la que va a girar el motor
+--StepEnable: switch que activa o no el movimiento del motor
+--CLK: para llevar un conteo de pasos 
+--StepDrive: las salidas de los 4 pines.
 
 architecture Behavioral of Motor_a_pasos is
-	
+	--Variables temporales:
 	signal aux : std_logic_vector (3 downto 0) := "0000";
 	signal state : std_logic_vector(1 downto 0) := "00";
 	signal StepCounter : std_logic_vector(31 downto 0) := (others => '0');
-	constant StepLockOut : std_logic_vector(31 downto 0) := "00000000010011000100101101000000";
+	constant StepLockOut : std_logic_vector(31 downto 0) := "00000000000001111010000100100000";
 	
 begin
 
---Estas variables temporales representan:
 --State: los posibles estados del motor.
---Contador de pasos: contador que aumenta cada vez que se realiza un paso.
---Steplock: indica la frecuencia a la cual el motor va a dar cada vuelta.
---Finalmente se tiene que hacer el process con el reloj y manipular los estados del motor.
-	StepDrive <= aux;
+--StepCounter: contador que aumenta cada vez que encuentra un flanco de subida en la señal de reloj.
+--StepLockOut: indica la frecuencia a la cual el motor va a dar cada paso.
+StepDrive <= aux;
 	process(CLK)
 		begin
-			if ((CLK'event) and (CLK='1')) then
-				StepCounter <= StepCounter + 1;
+			if ((CLK'event) and (CLK='1')) then		--Esto indica que cada vez que el reloj 
+StepCounter <= StepCounter + 1;	-- este en frente de subida se le 
+--aumentará en 1 a StepCounter
 
---Esto indica que cada vez que el reloj este en frente de subida se le aumentara en 1 al contador de pasos.
-
-				if (StepCounter >= StepLockOut) then
-						StepCounter <= (others => '0');
+				if (StepCounter >= StepLockOut) then			--Se resetea el contador
+						StepCounter <= (others => '0');		--si es mayor a la frecuencia
 						aux <= "1111";
 
---En esta parte solo se "resetea" el contador si es mayor a la frecuencia.
-
-						if (StepEnable = '1') then
+						if (StepEnable = '1') then		--Habilitador activado
 							if (Direction = '1') then state <= state + "01"; end if;
 							if (Direction = '0') then state <= state - "01"; end if;
-
---En este apartado se menciona hacia donde va a girar el motor según la variable direction.
-
-							case state is
+			
+							case state is		--Determina hacia dónde va a girar
 								when "00" =>aux <= "1000";
 								when "01" =>aux <= "0100";
 								when "10" =>aux <= "0010";
